d897a06e4beff5acc928205ac4a08ab803de59e8
Yustina Ovcharova <jusovch2000@gmail.com>
2021-03-18 20:43:24 +0200

    Delete redundant PermissionDenied test for getAllAliasesForUser

--- src/test/java/edu/kpi/testcourse/storage/UrlRepositoryFakeImplTest.java
+++ src/test/java/edu/kpi/testcourse/storage/UrlRepositoryFakeImplTest.java
@@ -43,11 +43,11 @@ class UrlRepositoryFakeImplTest {
   void shouldGetAllAliasesForUser() {
     //GIVEN
     UrlRepository repo = new UrlRepositoryFakeImpl();
-  
+
     //WHEN
     UrlAlias alias1 = new UrlAlias("http://r.com/short", "http://g.com/long1", "aaa@bbb.com");
     repo.createUrlAlias(alias1);
-  
+
     //THEN
     assertThat(repo.getAllAliasesForUser(alias1.email())).isNotNull();
   }
@@ -81,14 +81,16 @@ class UrlRepositoryFakeImplTest {
     UrlAlias alias1 = new UrlAlias("http://r.com/short", "http://g.com/long1", "aaa@bbb.com");
 
     //THEN
-    assertThrows(RuntimeException.class, ()->{repo.deleteUrlAlias(alias1.email(), alias1.alias());});
+    assertThrows(RuntimeException.class, () -> {
+      repo.deleteUrlAlias(alias1.email(), alias1.alias());
+    });
   }
 
   /**
    * Test if emails are not equals, throw PermissionDenied
    */
   @Test
-  void shouldCrashPermissionDenied() {
+  void shouldCrashOnDeletePermissionDenied() {
     //GIVEN
     UrlRepositoryFakeImpl repo = new UrlRepositoryFakeImpl();
 
@@ -97,19 +99,8 @@ class UrlRepositoryFakeImplTest {
     repo.createUrlAlias(alias1);
 
     //THEN
-    assertThrows(UrlRepository.PermissionDenied.class, ()->{repo.deleteUrlAlias("bbb@aaa.com", alias1.alias());});
-  }
-
-  @Test
-  void shouldGetAllAliasesForUser_throwPermissionDenied() {
-    //GIVEN
-    UrlRepository repo = new UrlRepositoryFakeImpl();
-
-    //WHEN
-    UrlAlias alias1 = new UrlAlias("http://r.com/short", "http://g.com/long1", "aaa@bbb.com");
-    repo.createUrlAlias(alias1);
-
-    //THEN
-    assertThrows(PermissionDenied.class, ()->{repo.getAllAliasesForUser("bbb@aaa.com");});
+    assertThrows(UrlRepository.PermissionDenied.class, () -> {
+      repo.deleteUrlAlias("bbb@aaa.com", alias1.alias());
+    });
   }
 }
